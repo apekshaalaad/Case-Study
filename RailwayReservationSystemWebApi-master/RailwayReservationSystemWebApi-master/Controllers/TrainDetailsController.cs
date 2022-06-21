765bf9b1fb7d707cd622e1ce0d09e77ecace810c
apekshaalaad <apekshalaad@gmail.com>
2022-06-21 11:21:45 +0530

    commit

---
+++ RailwayReservationSystemWebApi-master/RailwayReservationSystemWebApi-master/Controllers/TrainDetailsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Railway_Reservationsystem_WebAPI.Data;
+using Railway_Reservationsystem_WebAPI.Models;
+
+//Train Details Controller
+namespace Railway_Reservationsystem_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrainDetailsController : ControllerBase
+    {
+        private readonly LoginDbContext _context;
+        public TrainDetailsController(LoginDbContext loginDbContext)
+        {
+            _context = loginDbContext;
+
+        }
+        [HttpGet("TrainDetails")]
+        public IActionResult GetUsers()
+        {
+            var userdetails = _context.TrainDetailsModels.AsQueryable();
+            return Ok(new
+            {
+                StatusCode = 200,
+
+                TrainDetails = userdetails
+
+            });
+        }
+
+    }
+}
---
+++ RailwayReservationSystemWebApi-master/RailwayReservationSystemWebApi-master/Models/TrainDetails.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Railway_Reservationsystem_WebAPI.Models
+{
+    public class TrainDetails
+    {
+        [Key]
+        public int TrainId { get; set; }
+        public string? TrainName { get; set; }
+        public string? TrainType { get; set; }
+        public string? From { get; set; }
+        public string? TO { get; set; }
+        public string? ArrivalTime { get; set; }
+        public string? DepartureTime { get; set; }
+        public int Distance { get; set; }
+        public int TrainCharges { get; set; }
+
+    }
+}
